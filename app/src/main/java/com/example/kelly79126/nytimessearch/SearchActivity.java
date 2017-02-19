2ca2c5a3a77e7002707d4a50cae12e27775e04a6
kelly <kelly.chen@beseye.com>
2017-02-20 00:13:26 +0800

    first commit

---
+++ app/src/main/java/com/example/kelly79126/nytimessearch/SearchActivity.java
@@ -0,0 +1,57 @@
+package com.example.kelly79126.nytimessearch;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @BindView(R.id.etQuery) EditText etQuery;
+    @BindView(R.id.gvResults) GridView gvResults;
+    @BindView(R.id.btnSearch) Button btnSearch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onArticleSearch(View view) {
+        String query = etQuery.getText().toString();
+    }
+
+}
